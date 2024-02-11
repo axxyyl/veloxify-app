b258a6d066e0c7962c456631da12635969967954
Kiwi <allanvillegas35@gmail.com>
2024-02-11 15:48:47 +0800

    feat: added placeholder for  no avatars

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import logo from "../../../public/logo.png";
 import LinkIcon from "../icons/LinkIcon";
 import FolderIcon from "../icons/FolderIcon";
-const Navbar = () => {
+import { auth } from "@/auth";
+const Navbar = async () => {
+  const session = await auth();
   return (
     <>
       <nav className="border border-slate-200  rounded-full w-full p-3 flex justify-between gap-4">
@@ -39,14 +41,20 @@ const Navbar = () => {
           </Link>
         </div>
         <div>
-          <Image
-            width={15}
-            height={14}
-            src="https://scontent.fceb1-1.fna.fbcdn.net/v/t39.30808-6/426871024_3506019912948663_6564233975183278672_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeGQqQzhYRSgT9IvmOoHte8p2KNnZaOC_m7Yo2dlo4L-bnUo3SqNf60MZp3TNzUnPrqzpF4MOhg9QhN0PUrooo_n&_nc_ohc=iLPcJ3ebfBUAX8KrvkN&_nc_ht=scontent.fceb1-1.fna&oh=00_AfAzmeC82SPJMyO6BoryHQJK2ETq3qsP4CL2LPgWKjvM7Q&oe=65CC84D8"
-            alt="Profile Picture"
-            unoptimized={true}
-            className="rounded-full w-12 h-12"
-          />
+          {session?.user?.image === "" ? (
+            <p className="bg-black w-10 h-10 rounded-full text-white flex items-center justify-center text-xl">
+              {session?.user?.name ? session.user.name[0] : ""}
+            </p>
+          ) : (
+            <Image
+              width={15}
+              height={14}
+              src="https://scontent.fceb1-1.fna.fbcdn.net/v/t39.30808-6/426871024_3506019912948663_6564233975183278672_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeGQqQzhYRSgT9IvmOoHte8p2KNnZaOC_m7Yo2dlo4L-bnUo3SqNf60MZp3TNzUnPrqzpF4MOhg9QhN0PUrooo_n&_nc_ohc=iLPcJ3ebfBUAX8KrvkN&_nc_ht=scontent.fceb1-1.fna&oh=00_AfAzmeC82SPJMyO6BoryHQJK2ETq3qsP4CL2LPgWKjvM7Q&oe=65CC84D8"
+              alt="Profile Picture"
+              unoptimized={true}
+              className="rounded-full w-10 h-10"
+            />
+          )}
         </div>
       </nav>
     </>
