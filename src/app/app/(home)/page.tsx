5b0108672703f973c815311c2bf79fc506b3aad3
Kiwi <allanvillegas35@gmail.com>
2024-02-11 19:30:59 +0800

    feat: added profile summary

--- src/app/app/(home)/page.tsx
+++ src/app/app/(home)/page.tsx
@@ -1,40 +1,27 @@
 import { auth } from "../../../auth";
-import React from "react";
+import React, { Suspense } from "react";
 import Image from "next/image";
 import { signOut } from "../../../auth";
 import LogoutButton from "@/components/LogoutButton";
 import { connectToMongoDB } from "@/lib/db";
 import User from "@/models/userModel";
 import Navbar from "@/components/commons/Navbar";
+import AvatarIcon from "@/components/commons/AvatarIcon";
+import Link from "next/link";
+import ProfileSummary from "@/components/app/ProfileSummary";
 const page = async () => {
-  // const session = await auth();
-  // await connectToMongoDB();
-
-  // const user = await User.findOne({
-  //   email: session?.user?.email,
-  // });
-
-  console.log("render");
+  const session = await auth();
+  await connectToMongoDB();
 
   return (
     <>
-      asdsa
-      <div className="flex flex-col items-center gap-4">
-        {/* <Image
-          width={40}
-          height={20}
-          src="https://scontent.fceb1-1.fna.fbcdn.net/v/t39.30808-6/426871024_3506019912948663_6564233975183278672_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeGQqQzhYRSgT9IvmOoHte8p2KNnZaOC_m7Yo2dlo4L-bnUo3SqNf60MZp3TNzUnPrqzpF4MOhg9QhN0PUrooo_n&_nc_ohc=iLPcJ3ebfBUAX8KrvkN&_nc_ht=scontent.fceb1-1.fna&oh=00_AfAzmeC82SPJMyO6BoryHQJK2ETq3qsP4CL2LPgWKjvM7Q&oe=65CC84D8"
-          alt="Profile Picture"
-          unoptimized={true}
-          className="rounded-full w-40 h-40"
-        />
-        <h1 className="font-semibold">{session?.user?.name}</h1>
-        {user?.bio ? (
-          <p className="text-sm ">{user?.bio}</p>
-        ) : (
-          <p className="text-blue-600 cursor-pointer">Add a bio</p>
-        )} */}
-        <LogoutButton />
+      <div className="flex flex-col gap-10 items-start  w-full mt-10">
+        <h1>Welcome back, {session?.user?.name}!</h1>
+        <div>
+          <Suspense fallback={<span>Loading...</span>}>
+            <ProfileSummary session={session} />
+          </Suspense>
+        </div>
       </div>
     </>
   );
---
+++ src/components/app/ProfileSummary.tsx
@@ -0,0 +1,64 @@
+import User from "@/models/userModel";
+import Image from "next/image";
+import AvatarIcon from "../commons/AvatarIcon";
+import Link from "next/link";
+const ProfileSummary = async ({ session }: { session: any }) => {
+  const user = await User.findOne({
+    email: session?.user?.email,
+  });
+  return (
+    <>
+      <h1 className="font-bold">Profile Summary</h1>
+      <div className="mt-5"></div>
+      <div className=" w-full border border-slate-200 rounded-lg p-5 flex  flex-wrap flex-row items-center gap-10">
+        <div className="flex flex-col justify-center gap-x-2.5">
+          <div className="w-10 h-10">
+            {user && user?.avatar !== "" ? (
+              <Image
+                alt="Profile Pic"
+                src={user?.avatar || ""}
+                unoptimized={true}
+                className="h-10 w-10"
+              />
+            ) : (
+              <AvatarIcon name={user?.name} />
+            )}
+          </div>
+          <Link
+            href={`https://veloxify.vercel.app/${user?.username}`}
+            target="_blank"
+            className="text-sm text-center text-blue-500 "
+          >
+            View
+          </Link>
+        </div>
+        <div className="flex flex-col items-start">
+          <span className="text-sm text-slate-400">Name</span>
+          <span>{user?.name}</span>
+        </div>
+        <div className="flex flex-col items-start">
+          <span className="text-sm text-slate-400">Username</span>
+          <span>@{user?.username}</span>
+        </div>
+        <div className="flex flex-col items-start">
+          <span className="text-sm text-slate-400">Bio</span>
+          {user?.bio ? (
+            <span>{user?.bio}</span>
+          ) : (
+            <span className="text-sm text-blue-600">Add a bio</span>
+          )}
+        </div>
+        <div className="flex flex-col items-start">
+          <span className="text-sm text-slate-400">Email</span>
+          <span>{user?.email}</span>
+        </div>
+        <div className="flex flex-col items-start">
+          <span className="text-sm text-slate-400">Links</span>
+          <span>{user?.socials?.length}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileSummary;
