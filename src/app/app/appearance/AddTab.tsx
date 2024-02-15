827fcd4e71bde458fe69616b1cb6502e5e68e616
Kiwi <allanvillegas35@gmail.com>
2024-02-15 17:31:18 +0800

    ui: added ui for tab titels

--- src/app/app/appearance/AddTab.tsx
+++ src/app/app/appearance/AddTab.tsx
@@ -4,7 +4,16 @@ const AddTab = ({ tabs }: { tabs: any }) => {
   return (
     <div>
       <h1 className="text-xl font-bold mt-10 mb-5">Tabs</h1>
-      <div></div>
+      <div className="flex flex-col gap-4">
+        <div className="border border-gray-200 rounded-md flex justify-between items-center p-3">
+          <h1>My Recommendations</h1>
+          <span>x</span>
+        </div>
+        <div className="border border-gray-200 rounded-md flex justify-between items-center p-3">
+          <h1>My Recommendations</h1>
+          <span>x</span>
+        </div>
+      </div>
     </div>
   );
 };
