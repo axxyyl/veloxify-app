a342ee8f844553ecff5bdb1bd11613861479868e
Kiwi <allanvillegas35@gmail.com>
2024-02-17 19:52:59 +0800

    style: update responsiveness

--- src/app/app/appearance/Tabs.tsx
+++ src/app/app/appearance/Tabs.tsx
@@ -31,26 +31,27 @@ const Tabs = async ({ session }: { session: Session | null }) => {
                     </span>
                     <Link
                       href={"/"}
-                      className="text-blue-500 text-sm font-semibold"
+                      className="text-blue-500 text-sm font-semibold hover:underline"
                     >
                       {tab.name}
                     </Link>
                   </div>
-                  <p className="text-sm text-slate-600 mt-2 mb-1">
+                  <p className="text-xs text-slate-600 mt-2 mb-1 italic line-clamp-1">
                     {tab.description}
                   </p>
                   <span className="text-sm text-slate-500 flex gap-1 items-center">
-                    {" "}
-                    {tab.items?.length}{" "}
                     <span>
                       <ItemIcon />
                     </span>
+                    {tab.items?.length}{" "}
                   </span>
                 </div>
               </div>
-              <div className="flex items-center gap-4">
+              <div className="flex items-center gap-4 shrink-0">
                 <div className="flex flex-col text-sm">
-                  <span className="text-xs text-blue-500">View Tab</span>
+                  <span className="text-xs text-blue-500 cursor-pointer hover:text-blue-300">
+                    Add an item
+                  </span>
                 </div>
                 <CloseIcon />
               </div>
