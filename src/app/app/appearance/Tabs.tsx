fef03ecdd4ba9535824c615bb9c2872508e60b11
Kiwi <allanvillegas35@gmail.com>
2024-02-17 19:37:15 +0800

    ui: added some icons

--- src/app/app/appearance/Tabs.tsx
+++ src/app/app/appearance/Tabs.tsx
@@ -5,6 +5,9 @@ import { Session } from "next-auth";
 import AddTab from "./AddTab";
 import CloseIcon from "@/components/icons/CloseIcon";
 import DragIcon from "@/components/icons/DragIcon";
+import Link from "next/link";
+import TabIcon from "@/components/icons/TabIcon";
+import ItemIcon from "@/components/icons/ItemIcon";
 const Tabs = async ({ session }: { session: Session | null }) => {
   const tabs = await Tab.find({
     email: session?.user?.email,
@@ -13,20 +16,35 @@ const Tabs = async ({ session }: { session: Session | null }) => {
     <>
       <div className="relative mb-10">
         <h1 className="text-xl font-bold mt-10 mb-5">Tabs</h1>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 ">
           {tabs?.map((tab: ITabDocument) => (
             <div
               key={tab.name}
-              className="cursor-pointer border border-gray-200 rounded-md flex justify-between items-center p-3"
+              className=" border border-gray-200 rounded-md flex justify-between items-center p-3"
             >
               <div className="flex flex-row items-center gap-5">
                 <DragIcon />
                 <div className="flex flex-col">
-                  <h1 className="text-sm font-semibold">{tab.name}</h1>
-                  <p className="text-sm text-slate-600">{tab.description}</p>
-                  <span className="text-sm text-slate-500">
+                  <div className="flex gap-1 items-center">
+                    <span className="text-slate-600">
+                      <TabIcon />
+                    </span>
+                    <Link
+                      href={"/"}
+                      className="text-blue-500 text-sm font-semibold"
+                    >
+                      {tab.name}
+                    </Link>
+                  </div>
+                  <p className="text-sm text-slate-600 mt-2 mb-1">
+                    {tab.description}
+                  </p>
+                  <span className="text-sm text-slate-500 flex gap-1 items-center">
                     {" "}
-                    {tab.items?.length} Items
+                    {tab.items?.length}{" "}
+                    <span>
+                      <ItemIcon />
+                    </span>
                   </span>
                 </div>
               </div>
